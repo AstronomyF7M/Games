74cd3709db0337d1abf6cca59049c3ef5a0bf4f3
AstroV3 <8769937516@student.cms.k12.nc.us>
2025-05-19 14:34:50 -0400

    Create ComingSoon.jsx

---
+++ ComingSoon.jsx
@@ -0,0 +1,10 @@
+// src/components/CircuitBreakers.jsx
+export default function CircuitBreakers() {
+  return (
+    <div className="neon-text" style={{ fontSize: 36, marginTop: 80, textAlign: "center" }}>
+    SOMETHING BIG IS COMING SOON 
+      CIRCUITS ARE BREAKING
+    </div>
+  );
+}
+// Repeat for other games: LaserDiscDuel.jsx, CosmicPinball.jsx, SynthwaveSmash.jsx, PixelHeist.jsx
